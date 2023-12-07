c7b2edb2258c2c8e92b1b77e4f1b86287e44c118
bvivar01 <80649081+bvivar01@users.noreply.github.com>
2023-12-07 15:06:59 -0800

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+ull#!/bin/bash
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull bvivar125/sample-python-flask-project
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000
