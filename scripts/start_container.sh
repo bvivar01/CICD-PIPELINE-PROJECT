fc15ff0603bb21b5b42aad6a0d9ee55e8b15f790
bvivar01 <80649081+bvivar01@users.noreply.github.com>
2023-12-07 17:50:12 -0800

    changed port

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull bvivar125/sample-python-flask-project
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 bvivar125/sample-python-flask-project
+docker run -d -p 8000:8000 bvivar125/sample-python-flask-project
